92011270697eb34568bdf9501b38542dd9470dcd
Geir André Lund <geir.andre.lund@nav.no>
2019-03-21 07:49:39 +0100

    Wip - naming rules

--- src/main/kotlin/no/nav/dagpenger/regel/minsteinntekt/InngangsvilkårSpesifikasjoner.kt
+++ src/main/kotlin/no/nav/dagpenger/regel/minsteinntekt/InngangsvilkårSpesifikasjoner.kt
@@ -2,81 +2,79 @@ package no.nav.dagpenger.regel.minsteinntekt
 
 import no.nav.nare.core.evaluations.Evaluering
 import no.nav.nare.core.specifications.Spesifikasjon
-import java.math.BigDecimal
 
-val minsteinntektEtterAvtjentVerneplikt: Spesifikasjon<Fakta> = Spesifikasjon(
+// https://www.nav.no/rettskildene/lov/L19970228-19_P4-18#L19970228-19_P4-18
+// https://www.nav.no/rettskildene/lov/L19970228-19_P4-4#L19970228-19_P4-4
+// https://www.nav.no/rettskildene/lov/L19970228-19_P4-4#L19970228-19_P4-19
+
+internal val verneplikt: Spesifikasjon<Fakta> = Spesifikasjon(
     beskrivelse = "§ 4-19 Dagpenger etter avtjent verneplikt",
-    identitet = "MINSTEINNTEKT_V1_5",
+    identitet = "§ 4-19",
     implementasjon = { fakta ->
         if (fakta.verneplikt) {
-            Evaluering.ja("Rett § 4-19 Dagpenger etter avtjent verneplikt ")
+            Evaluering.ja("Oppfylt etter § 4-19 Dagpenger etter avtjent verneplikt")
         } else {
-            Evaluering.nei("Ikke rett § 4-19 Dagpenger etter avtjent verneplikt ")
+            Evaluering.nei("Ikke oppfylt etter § 4-19 Dagpenger etter avtjent verneplikt")
         }
     }
 )
 
-val ordinærSiste12Måneder = Spesifikasjon<Fakta>(
-    beskrivelse = "§ 4-4. Krav til minsteinntekt, siste 12 måneder",
-    identitet = "ORDINÆR_12",
+internal val ordinærSiste12Måneder = Spesifikasjon<Fakta>(
+    beskrivelse = "§ 4-4. Krav til minsteinntekt, minst 1,5 ganger grunnbeløp siste 12 måneder",
+    identitet = "§ 4-4 12mnd",
     implementasjon = { fakta ->
-        if (fakta.arbeidsinntektSiste12 > (fakta.grunnbeløp.times(BigDecimal(1.5)))) {
-            Evaluering.ja("Rett til Dagpenger etter § 4-4. Krav til minsteinntekt, siste 12 måneder")
+        if (fakta.arbeidsinntektSiste12 > (fakta.grunnbeløp.times(1.5.toBigDecimal()))) {
+            Evaluering.ja("Oppfylt etter § 4-4. Krav til minsteinntekt - minst 1,5 ganger grunnbeløp siste 12 måneder")
         } else {
-            Evaluering.nei("Ikke rett til Dagpenger etter § 4-4. Krav til minsteinntekt, siste 12 måneder")
+            Evaluering.nei("Ikke Oppfylt etter § 4-4. Krav til minsteinntekt - minst 1,5 ganger grunnbeløp siste 12 måneder")
         }
     }
 )
 
-val ordinærSiste36Måneder = Spesifikasjon<Fakta>(
-    beskrivelse = "§ 4-4. Krav til minsteinntekt, siste 36 måneder",
-    identitet = "ORDINÆR_36",
+internal val ordinærSiste36Måneder = Spesifikasjon<Fakta>(
+    beskrivelse = "§ 4-4. Krav til minsteinntekt, minst 3 ganger grunnbeløpet siste 36 måneder",
+    identitet = "§ 4-4 36mnd",
     implementasjon = { fakta ->
-        if (fakta.arbeidsinntektSiste36 > (fakta.grunnbeløp.times(BigDecimal(3)))) {
-            Evaluering.ja("Rett til Dagpenger etter § 4-4. Krav til minsteinntekt, siste 36 måneder")
+        if (fakta.arbeidsinntektSiste36 > (fakta.grunnbeløp.times(3.toBigDecimal()))) {
+            Evaluering.ja("Oppfylt etter § 4-4. Minsteinntekt - minst 3 ganger grunnbeløpet siste 36 måneder")
         } else {
-            Evaluering.nei("Ikke rett til Dagpenger etter § 4-4. Krav til minsteinntekt, siste 36 måneder")
+            Evaluering.nei("Ikke oppfylt etter § 4-4. Minsteinntekt - minst 3 ganger grunnbeløpet siste 36 måneder")
         }
     }
 )
 
-val ordinærSiste12MånederMedFangstOgFiske = Spesifikasjon<Fakta>(
-    beskrivelse = "§ 4-4. Krav til minsteinntekt, siste 12 måneder, etter næringsinntekt",
-    identitet = "ORDINÆR_12_NÆRING",
+internal val ordinærSiste12MånederMedFangstOgFiske = Spesifikasjon<Fakta>(
+    beskrivelse = "§ 4-18 - Fangs og Fiske - minst 1,5 ganger grunnbeløp siste 12 måneder",
+    identitet = "§ 4-18 12mnd",
     implementasjon = { fakta ->
-        if (fakta.fangstOgFisk && fakta.inntektSiste12inkludertFangstOgFiske > (fakta.grunnbeløp.times(BigDecimal(1.5)))) {
-            Evaluering.ja("Rett til Dagpenger etter § 4-4. Krav til minsteinntekt, siste 12 måneder, etter næringsinntekt")
+        if (fakta.fangstOgFisk && fakta.inntektSiste12inkludertFangstOgFiske > (fakta.grunnbeløp.times(1.5.toBigDecimal()))) {
+            Evaluering.ja("Oppfylt etter § 4-18 - Fangs og Fiske - minst 1,5 ganger grunnbeløp siste 12 måneder")
         } else {
-            Evaluering.nei("Ikke rett til Dagpenger etter § 4-4. Krav til minsteinntekt, siste 12 måneder, etter næringsinntekt")
+            Evaluering.nei("Ikke oppfylt etter § 4-18 - Fangs og Fiske - minst 1,5 ganger grunnbeløp siste 12 måneder")
         }
     }
 )
 
-val ordinærSiste36MånederMedFangstOgFiske = Spesifikasjon<Fakta>(
-    beskrivelse = "§ 4-4. Krav til minsteinntekt, siste 12 måneder, etter næringsinntekt",
-    identitet = "ORDINÆR_36_NÆRING",
+internal val ordinærSiste36MånederMedFangstOgFiske = Spesifikasjon<Fakta>(
+    beskrivelse = "§ 4-18 - Fangs og Fiske -, minst 3 ganger grunnbeløpet siste 36 måneder",
+    identitet = "§ 4-18 36mnd",
     implementasjon = { fakta ->
-        if (fakta.fangstOgFisk && fakta.inntektSiste36inkludertFangstOgFiske > (fakta.grunnbeløp.times(BigDecimal(3)))) {
-            Evaluering.ja("Rett til Dagpenger etter § 4-4. Krav til minsteinntekt, siste 12 måneder, etter næringsinntekt")
+        if (fakta.fangstOgFisk && fakta.inntektSiste36inkludertFangstOgFiske > (fakta.grunnbeløp.times(3.toBigDecimal()))) {
+            Evaluering.ja("Oppfylt etter § 4-18 - Fangs og Fiske - minst 3 ganger grunnbeløpet siste 36 måneder")
         } else {
-            Evaluering.nei("Ikke rett til Dagpenger etter § 4-4. Krav til minsteinntekt, siste 12 måneder, etter næringsinntekt")
+            Evaluering.nei("Ikke oppfylt etter § 4-18 - Fangs og Fiske - minst 3 ganger grunnbeløpet siste 36 måneder")
         }
     }
 )
 
-val ordninær: Spesifikasjon<Fakta> =
+internal val ordinær: Spesifikasjon<Fakta> =
     (ordinærSiste12Måneder eller ordinærSiste36Måneder).eller(ordinærSiste12MånederMedFangstOgFiske eller ordinærSiste36MånederMedFangstOgFiske)
         .med(
-            identitet = "ORDINÆR",
-            beskrivelse = "§ 4-4. Krav til minsteinntekt etter ordinær inntekt oppfyllt?"
+            identitet = "§ 4-4 § 4-18",
+            beskrivelse = "§ 4-4 eller § 4-18 Krav til minsteinntekt etter ordinær/fangs og fiske inntekt oppfyllt?"
         )
 
-val verneplikt: Spesifikasjon<Fakta> = minsteinntektEtterAvtjentVerneplikt.med(
-    identitet = "VERNEPLIKT",
-    beskrivelse = "§ 4-19. Dagpenger etter avtjent verneplikt"
-)
-
-val inngangsVilkår: Spesifikasjon<Fakta> = (ordninær eller verneplikt).med(
-    identitet = "INNGANGSVILKÅR",
-    beskrivelse = "§ 4-4. eller § 4-19. inngangsvilkår oppfyllt?"
+internal val inngangsVilkår: Spesifikasjon<Fakta> = (ordinær eller verneplikt).med(
+    identitet = "urn:dp:iv:minsteinntekt:verneplikt:v1",
+    beskrivelse = "§ 4-4 og § 4-18 eller § 4-19. inngangsvilkår oppfyllt?"
 )
--- src/test/kotlin/no/nav/dagpenger/regel/minsteinntekt/InngangsvilkårTest.kt
+++ src/test/kotlin/no/nav/dagpenger/regel/minsteinntekt/InngangsvilkårTest.kt
@@ -3,17 +3,17 @@ package no.nav.dagpenger.regel.minsteinntekt
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
-internal class InngångsvilkårTest {
+internal class InngangsvilkårTest {
 
     @Test
     fun `Inngångsvilkår består av Rett § 4-19 Dagpenger etter avtjent verneplikt  og § 4-4 minsteinntekt `() {
-        assertEquals("ORDINÆR, VERNEPLIKT", inngangsVilkår.children.joinToString { it.identitet })
+        assertEquals("§ 4-4 § 4-18, § 4-19", inngangsVilkår.children.joinToString { it.identitet })
     }
 
     @Test
     fun ` Minsteinntekt ordinær består § 4-4 minsteinntekt `() {
         assertEquals(
-            "ORDINÆR_12 ELLER ORDINÆR_36, ORDINÆR_12_NÆRING ELLER ORDINÆR_36_NÆRING",
-            ordninær.children.joinToString { it.identitet })
+            "§ 4-4 12mnd ELLER § 4-4 36mnd, § 4-18 12mnd ELLER § 4-18 36mnd",
+            ordinær.children.joinToString { it.identitet })
     }
 }
