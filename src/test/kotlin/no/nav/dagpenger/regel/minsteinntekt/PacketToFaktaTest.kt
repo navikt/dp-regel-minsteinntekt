62bd5c74fd56364834e231ba6be6bacd80ca7979
Tonje Mjøvik <tonje.mjovik@nav.no>
2019-04-23 12:47:36 +0200

    test bruktInntektsperiode

--- src/test/kotlin/no/nav/dagpenger/regel/minsteinntekt/PacketToFaktaTest.kt
+++ src/test/kotlin/no/nav/dagpenger/regel/minsteinntekt/PacketToFaktaTest.kt
@@ -2,8 +2,10 @@ package no.nav.dagpenger.regel.minsteinntekt
 
 import no.nav.dagpenger.events.Packet
 import no.nav.dagpenger.events.inntekt.v1.Inntekt
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
+import java.time.YearMonth
 
 class PacketToFaktaTest {
 
@@ -42,5 +44,22 @@ class PacketToFaktaTest {
         val fakta = packetToFakta(packet)
 
         assertTrue(fakta.verneplikt)
+    }
+
+    @Test
+    fun ` should map brukt_inntektsperiode from packet to Fakta `() {
+        val json = """
+        {
+            "senesteInntektsmåned":"2018-03",
+            "bruktInntektsPeriode": {"førsteMåned":"2019-02", "sisteMåned":"2019-03"}
+        }""".trimIndent()
+
+        val packet = Packet(json)
+        packet.putValue("inntektV1", MinsteinntektTopologyTest.jsonAdapterInntekt.toJsonValue(emptyInntekt)!!)
+
+        val fakta = packetToFakta(packet)
+
+        assertEquals(YearMonth.of(2019, 2), fakta.bruktInntektsPeriode!!.førsteMåned)
+        assertEquals(YearMonth.of(2019, 3), fakta.bruktInntektsPeriode!!.sisteMåned)
     }
 }
