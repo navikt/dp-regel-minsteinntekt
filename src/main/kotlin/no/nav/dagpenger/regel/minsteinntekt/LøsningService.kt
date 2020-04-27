d7dc898d7cf5350f69e278c0e83586ec8babb013
bendreas <andreas.nygard.bergh@nav.no>
2020-04-27 12:06:44 +0200

    log onError

--- src/main/kotlin/no/nav/dagpenger/regel/minsteinntekt/LøsningService.kt
+++ src/main/kotlin/no/nav/dagpenger/regel/minsteinntekt/LøsningService.kt
@@ -1,11 +1,13 @@
 package no.nav.dagpenger.regel.minsteinntekt
 
+import mu.KotlinLogging
 import no.nav.dagpenger.regel.minsteinntekt.Minsteinntekt.Companion.AVTJENT_VERNEPLIKT
 import no.nav.dagpenger.regel.minsteinntekt.Minsteinntekt.Companion.BRUKT_INNTEKTSPERIODE
 import no.nav.dagpenger.regel.minsteinntekt.Minsteinntekt.Companion.FANGST_OG_FISK
 import no.nav.dagpenger.regel.minsteinntekt.Minsteinntekt.Companion.INNTEKT
 import no.nav.dagpenger.regel.minsteinntekt.Minsteinntekt.Companion.LÆRLING
 import no.nav.helse.rapids_rivers.JsonMessage
+import no.nav.helse.rapids_rivers.MessageProblems
 import no.nav.helse.rapids_rivers.RapidsConnection
 import no.nav.helse.rapids_rivers.River
 import no.nav.nare.core.evaluations.Evaluering
@@ -14,6 +16,9 @@ import no.nav.nare.core.evaluations.Resultat
 class LøsningService(
     rapidsConnection: RapidsConnection
 ) : River.PacketListener {
+    private val log = KotlinLogging.logger {}
+    private val sikkerlogg = KotlinLogging.logger("tjenestekall")
+
     init {
         River(rapidsConnection).apply {
             validate { it.requireAll("@behov", listOf(MINSTEINNTEKT)) }
@@ -31,6 +36,11 @@ class LøsningService(
     override fun onPacket(packet: JsonMessage, context: RapidsConnection.MessageContext) {
         val packetMedLøsning = løsFor(packet)
         context.send(packetMedLøsning.toJson())
+    }
+
+    override fun onError(problems: MessageProblems, context: RapidsConnection.MessageContext) {
+        log.info { problems.toString() }
+        sikkerlogg.info { problems.toExtendedReport() }
     }
 
     private fun løsFor(packet: JsonMessage): JsonMessage {
