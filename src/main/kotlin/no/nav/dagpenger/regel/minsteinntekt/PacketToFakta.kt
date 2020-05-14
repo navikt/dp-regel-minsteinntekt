7857711ab5b0dc29246999a62735e97b9870ff2b
Geir André Lund <geir.andre.lund@nav.no>
2020-05-14 12:16:43 +0200

    Hent inntekt fra dp-inntekt-api-gprc basert på inntektsid
    resolves navikt/dagpenger#415

--- src/main/kotlin/no/nav/dagpenger/regel/minsteinntekt/PacketToFakta.kt
+++ src/main/kotlin/no/nav/dagpenger/regel/minsteinntekt/PacketToFakta.kt
@@ -1,14 +1,13 @@
 package no.nav.dagpenger.regel.minsteinntekt
 
 import com.fasterxml.jackson.databind.JsonNode
-import java.math.BigDecimal
+import kotlinx.coroutines.runBlocking
 import no.nav.dagpenger.events.Packet
 import no.nav.dagpenger.events.inntekt.v1.Inntekt
-import no.nav.dagpenger.events.inntekt.v1.InntektKlasse
-import no.nav.dagpenger.events.inntekt.v1.KlassifisertInntekt
-import no.nav.dagpenger.events.inntekt.v1.KlassifisertInntektMåned
+import no.nav.dagpenger.inntekt.rpc.InntektHenter
 import no.nav.dagpenger.regel.minsteinntekt.Application.Companion.BEREGNINGSDATO_GAMMEL_SKRIVEMÅTE
 import no.nav.dagpenger.regel.minsteinntekt.LøsningService.Companion.BEREGNINGSDATO_NY_SRKIVEMÅTE
+import no.nav.dagpenger.regel.minsteinntekt.LøsningService.Companion.INNTEKT_ID
 import no.nav.dagpenger.regel.minsteinntekt.Minsteinntekt.Companion.AVTJENT_VERNEPLIKT
 import no.nav.dagpenger.regel.minsteinntekt.Minsteinntekt.Companion.BRUKT_INNTEKTSPERIODE
 import no.nav.dagpenger.regel.minsteinntekt.Minsteinntekt.Companion.FANGST_OG_FISK
@@ -48,24 +47,8 @@ internal fun packetToFakta(packet: Packet): Fakta {
     )
 }
 
-internal fun JsonMessage.toFakta(): Fakta {
-    val inntekt = this[INNTEKT].let {
-        Inntekt(
-            inntektsId = it["inntektsId"].asText(),
-            inntektsListe = it["inntektsListe"].map {
-                KlassifisertInntektMåned(
-                    årMåned = it["årMåned"].asYearMonth(),
-                    klassifiserteInntekter = it["klassifiserteInntekter"].map {
-                        KlassifisertInntekt(
-                            beløp = BigDecimal(
-                                it["beløp"].asInt()
-                            ), inntektKlasse = InntektKlasse.valueOf(it["inntektKlasse"].asText())
-                        )
-                    })
-            }, manueltRedigert = it["manueltRedigert"].asBoolean(),
-            sisteAvsluttendeKalenderMåned = it["sisteAvsluttendeKalenderMåned"].asYearMonth()
-        )
-    }
+internal fun JsonMessage.toFakta(inntektHenter: InntektHenter): Fakta {
+    val inntekt = this[INNTEKT_ID].asText().let { runBlocking { inntektHenter.hentKlassifisertInntekt(it) } }
     val avtjentVerneplikt = this[AVTJENT_VERNEPLIKT].asBoolean(false)
     val bruktInntektsPeriode =
         this["bruktInntektsPeriode"].takeIf(JsonNode::isObject)
