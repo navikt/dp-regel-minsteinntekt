1ab1e48a64db4036c3b575b74036db6a2483dffd
Geir André Lund <geir.andre.lund@nav.no>
2026-04-15 09:15:38 +0200

    Ny verdi og dato for gjusteringstest.

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,13 @@ repositories {
     mavenCentral()
     maven("https://jitpack.io")
     maven("https://github-package-registry-mirror.gc.nav.no/cached/maven-release")
+    maven {
+        url = uri("https://maven.pkg.github.com/navikt/maven-release")
+        credentials {
+            username = System.getenv("GITHUB_USERNAME")
+            password = System.getenv("GITHUB_PASSWORD")
+        }
+    }
 }
 
 application {
